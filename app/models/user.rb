1e2f0725cac22756096366f1e50cd84a2b6eb55d
map3101 <marcelo.postiglione@gmail.com>
2020-12-15 20:24:08 -0300

    feat: cria associations para a model user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,16 @@
 class User < ApplicationRecord
-    #has_many :comments
-    #has_many :likes
-    #has_many :replies
+    # Associations
+    has_many :comments
+    has_many :likes
+    has_many :replies
 
+    # Criptografar a senha
     has_secure_password
 
+    # Validações
     validates :name, presence: true
     validates :email, presence: true, uniqueness: true
 
+    # Active Storage para profile picture
     has_one_attached :image
 end
