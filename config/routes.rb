6aa7d66fbf2128b562a13b82f5804681b871e6d6
map3101 <marcelo.postiglione@gmail.com>
2020-12-15 21:58:44 -0300

    feat: cria actions e rotas para os contacts

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  get "/messages", to: "contacts#index"
+  get "/messages/:id", to: "contacts#show"
+  post "/messages", to: "contacts#create"
+  delete "/messages/:id", to: "contacts#destroy"
+
 end
