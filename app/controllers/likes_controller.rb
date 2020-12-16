91bf412686be138afc8822efe5d07749edacaa10
map3101 <marcelo.postiglione@gmail.com>
2020-12-16 03:13:56 -0300

    feat: cria actions para realizar queries na database

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,5 +1,6 @@
 class LikesController < ApplicationController
-    before_action :set_like, only: [:userlikes, :postlikes]
+    before_action :set_userlike, only: [:userlikes]
+    before_action :set_postlike, only: [:postlikes]
 
     # GET /likes Mostra todos os likes
     def index
@@ -7,14 +8,14 @@ class LikesController < ApplicationController
         render json: @likes, status: 200
     end
 
-    # GET /likes/:user_id Mostra os likes do usuário com id especificada
+    # GET /likes//user/:user_id Mostra os likes do usuário com id especificada
     def userlikes
-        render json: @like, status: 200
+        render json: @likes, status: 200
     end
 
-    # GET /likes/:post_id Mostra os likes no post com id especificada
+    # GET /likes/post/:post_id Mostra os likes no post com id especificada
     def postlikes
-        render json: @like, status: 200
+        render json: @likes, status: 200
     end
 
     # POST /likes Cria uma nova mensagem
@@ -29,10 +30,16 @@ class LikesController < ApplicationController
 
     private
 
-    def set_like
-        @like = Like.find(params[:id])
+    # Retorna os likes de um usuário 
+    def set_userlike
+        @likes = Like.where(user_id: params[:user_id])
     end
 
+    # Retorna os likes de uma postagem
+    def set_postlike
+        @likes = Like.where(post_id: params[:post_id])
+    end
+    
     def like_params
         params.require(:like).permit(:post_id, :user_id)
     end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "/likes", to: "likes#index"
-  get "/likes/:user_id", to: "likes#userlikes"
-  get "/likes/:id", to: "likes#postlikes"
+  get "/likes/user/:user_id", to: "likes#userlikes"
+  get "/likes/post/:post_id", to: "likes#postlikes"
   post "/likes", to: "likes#create"
 end
