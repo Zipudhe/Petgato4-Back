659fd7e970875b5d527ad262fecae711480642de
map3101 <marcelo.postiglione@gmail.com>
2021-01-18 15:03:58 -0300

    fix: altera pedido de autorização para requests no user e altera tempo de validade do token

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-    before_action :authorize_request, except: :create
+    before_action :authorize_request, only: [:index, :update, :destroy]
     before_action :set_user, only: [:show, :update, :destroy]
 
     # GET /users all users
