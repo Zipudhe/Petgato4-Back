6eb10b8e902751b164fe812f2609d36e89fcc09a
map3101 <marcelo.postiglione@gmail.com>
2021-01-20 11:15:02 -0300

    tarefa: impede criação de reports repetidos

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,6 +1,6 @@
 class Report < ApplicationRecord
-    validates :reply_id, presence: true
-    validates :comment_id, presence: true
+    validates :reply_id, presence: true, uniqueness: true
+    validates :comment_id, presence: true, uniqueness: true
 
     has_one :reply
     has_one :comment
