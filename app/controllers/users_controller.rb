1439d48b61f3e85d70dafbf3e0378bf1c0ea0985
Victor Manuel <victormanuel@cjr.org.br>
2021-01-21 22:29:24 -0300

    fix: adiciona autorização ao alterar usuário

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-    #before_action :authorize_request, only: [:update, :destroy]
+    before_action :authorize_request, only: [:update, :destroy]
     before_action :set_user, only: [:show, :update, :destroy]
 
     USERS_PER_PAGE = 5
