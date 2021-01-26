227d204f4cef79f7c63083ea3b111647f8ac67a2
map3101 <marcelo.postiglione@gmail.com>
2021-01-26 14:45:33 -0300

    feat: adiciona requisitos para a senha, 8 caracteres de tamanho e 1 número

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,9 +7,16 @@ class User < ApplicationRecord
     # Criptografar a senha
     has_secure_password
 
+    # Requisitos da senha
+    PASSWORD_REQUIREMENTS = /\A
+        (?=.{8,})
+        (?=.*\d)
+    /x
+
     # Validações
     validates :name, presence: true
     validates :email, presence: true, uniqueness: true
+    validates :password, format: PASSWORD_REQUIREMENTS
 
     # Active Storage para profile picture
     has_one_attached :profile_image
